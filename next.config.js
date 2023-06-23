fc2f0914ca55ff66decfbb37b2a5e71334daf127
Cluster-Taek <yst811@naver.com>
2023-06-23 10:27:17 +0900

    build: add pwa initial setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa');
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+  reactStrictMode: true,
+};
+
+const nextConfig = withPWA({
+  dest: 'public',
+  // disable: !isProduction,
+  runtimeCaching: [],
+})(config);
+
+module.exports = nextConfig;
