cbcf77ab6083962b8ee84fc7c862e12929947236
Cluster-Taek <yst811@naver.com>
2023-07-05 11:34:37 +0900

    chore: delete next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,15 @@
 import '@/styles/base.css';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
 import Modal from '@/components/common/Modal';
 import Toast from '@/components/common/Toast';
 import FloatingProvider from '@/context/FloatingContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <FloatingProvider>
-        <Component {...pageProps} />
-        <Modal />
-        <Toast />
-      </FloatingProvider>
-    </SessionProvider>
+    <FloatingProvider>
+      <Component {...pageProps} />
+      <Modal />
+      <Toast />
+    </FloatingProvider>
   );
 }
