0803aa0462dcc1c5a965311f89958088e7146679
Cluster-Taek <yst811@naver.com>
2023-09-21 17:40:47 +0900

    feat: move install prompt app to main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,6 @@ import Head from 'next/head';
 import { useState } from 'react';
 import Modal from '@/components/common/Modal';
 import Toast from '@/components/common/Toast';
-import InstallPrompt from '@/components/main/InstallPrompt';
 import { AuthProvider } from '@/context/AuthContext';
 import FloatingProvider from '@/context/FloatingContext';
 import 'dayjs/locale/ko';
@@ -36,7 +35,6 @@ export default function App({ Component, pageProps }: AppProps) {
                 <Component {...pageProps} />
                 <Modal />
                 <Toast />
-                <InstallPrompt />
               </main>
             </AuthProvider>
           </FloatingProvider>
